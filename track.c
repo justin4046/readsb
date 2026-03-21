63f526e7db61d8c9ecdb441026876359b0b1634f
justin4046 <71111866+justin4046@users.noreply.github.com>
2026-03-20 21:49:54 -0500

    message fixes

--- track.c
+++ track.c
@@ -2195,6 +2195,7 @@ struct aircraft *trackUpdateFromMessage(struct modesMessage *mm) {
 
     switch (mm->source) {
     case SOURCE_ADSB:
+    case SOURCE_UAT:
         message_version = &a->adsb_version;
         break;
     case SOURCE_TISB:
